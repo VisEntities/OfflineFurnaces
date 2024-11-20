9fdc7a42ccc61675162e1c0675069aaca998fb13
VisEntities <visentities@gmail.com>
2024-11-20 19:29:49 +0100

    Add project files.

---
+++ OfflineFurnaces.cs
@@ -0,0 +1,239 @@
+﻿/*
+ * Copyright (C) 2024 Game4Freak.io
+ * This mod is provided under the Game4Freak EULA.
+ * Full legal terms can be found at https://game4freak.io/eula/
+ */
+
+using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Oxide.Plugins
+{
+    [Info("Offline Furnaces", "VisEntities", "1.0.0")]
+    [Description(" ")]
+    public class OfflineFurnaces : RustPlugin
+    {
+        #region Fields
+
+        private static OfflineFurnaces _plugin;
+        private static Configuration _config;
+
+        #endregion Fields
+
+        #region Configuration
+
+        private class Configuration
+        {
+            [JsonProperty("Version")]
+            public string Version { get; set; }
+
+            [JsonProperty("Oven Short Prefab Names")]
+            public List<string> OvenShortPrefabNames { get; set; }
+        }
+
+        protected override void LoadConfig()
+        {
+            base.LoadConfig();
+            _config = Config.ReadObject<Configuration>();
+
+            if (string.Compare(_config.Version, Version.ToString()) < 0)
+                UpdateConfig();
+
+            SaveConfig();
+        }
+
+        protected override void LoadDefaultConfig()
+        {
+            _config = GetDefaultConfig();
+        }
+
+        protected override void SaveConfig()
+        {
+            Config.WriteObject(_config, true);
+        }
+
+        private void UpdateConfig()
+        {
+            PrintWarning("Config changes detected! Updating...");
+
+            Configuration defaultConfig = GetDefaultConfig();
+
+            if (string.Compare(_config.Version, "1.0.0") < 0)
+                _config = defaultConfig;
+
+            PrintWarning("Config update complete! Updated from version " + _config.Version + " to " + Version.ToString());
+            _config.Version = Version.ToString();
+        }
+
+        private Configuration GetDefaultConfig()
+        {
+            return new Configuration
+            {
+                Version = Version.ToString(),
+                OvenShortPrefabNames = new List<string>
+                {
+                    "furnace",
+                    "legacy_furnace",
+                    "furnace.large",
+                    "electricfurnace.deployed"
+                },
+            };
+        }
+
+        #endregion Configuration
+
+        #region Oxide Hooks
+
+        private void Init()
+        {
+            _plugin = this;
+            PermissionUtil.RegisterPermissions();
+        }
+
+        private void Unload()
+        {
+            CoroutineUtil.StopAllCoroutines();
+            _config = null;
+            _plugin = null;
+        }
+
+        private void OnPlayerDisconnected(BasePlayer player, string reason)
+        {
+            if (player == null)
+                return;
+
+            if (PermissionUtil.HasPermission(player, PermissionUtil.IGNORE))
+                return;
+
+            RelationshipManager.PlayerTeam team = PlayerUtil.GetTeam(player.userID);
+            if (team != null)
+            {
+                foreach (ulong memberId in team.members)
+                {
+                    if (!PlayerUtil.Offline(memberId))
+                        return;
+                }
+            }
+
+            CoroutineUtil.StartCoroutine(Guid.NewGuid().ToString(), TurnOffOvensCoroutine(player, team));
+        }
+
+        #endregion Oxide Hooks
+
+        #region Oven Shutdown Logic
+
+        private IEnumerator TurnOffOvensCoroutine(BasePlayer disconnectingPlayer, RelationshipManager.PlayerTeam team)
+        {
+            HashSet<ulong> playerIds = new HashSet<ulong>
+            {
+                disconnectingPlayer.userID
+            };
+
+            if (team != null)
+                playerIds.UnionWith(team.members);
+
+            foreach (BaseOven oven in BaseNetworkable.serverEntities.OfType<BaseOven>())
+            {
+                if (oven != null
+                     && _config.OvenShortPrefabNames.Contains(oven.ShortPrefabName)
+                     && oven.OwnerID != 0
+                     && playerIds.Contains(oven.OwnerID))
+                {
+                    oven.StopCooking();
+                }
+
+                yield return null;
+            }
+        }
+
+        #endregion Oven Shutdown Logic
+
+        #region Helper Classes
+
+        public static class PlayerUtil
+        {
+            public static BasePlayer FindById(ulong playerId)
+            {
+                return RelationshipManager.FindByID(playerId);
+            }
+
+            public static RelationshipManager.PlayerTeam GetTeam(ulong playerId)
+            {
+                if (RelationshipManager.ServerInstance == null)
+                    return null;
+
+                return RelationshipManager.ServerInstance.FindPlayersTeam(playerId);
+            }
+
+            public static bool Offline(ulong playerId)
+            {
+                BasePlayer player = FindById(playerId);
+                return player == null || !player.IsConnected;
+            }
+        }
+
+        public static class CoroutineUtil
+        {
+            private static readonly Dictionary<string, Coroutine> _activeCoroutines = new Dictionary<string, Coroutine>();
+
+            public static void StartCoroutine(string coroutineName, IEnumerator coroutineFunction)
+            {
+                StopCoroutine(coroutineName);
+
+                Coroutine coroutine = ServerMgr.Instance.StartCoroutine(coroutineFunction);
+                _activeCoroutines[coroutineName] = coroutine;
+            }
+
+            public static void StopCoroutine(string coroutineName)
+            {
+                if (_activeCoroutines.TryGetValue(coroutineName, out Coroutine coroutine))
+                {
+                    if (coroutine != null)
+                        ServerMgr.Instance.StopCoroutine(coroutine);
+
+                    _activeCoroutines.Remove(coroutineName);
+                }
+            }
+
+            public static void StopAllCoroutines()
+            {
+                foreach (string coroutineName in _activeCoroutines.Keys.ToArray())
+                {
+                    StopCoroutine(coroutineName);
+                }
+            }
+        }
+
+        #endregion Helper Classes
+
+        #region Permissions
+
+        private static class PermissionUtil
+        {
+            public const string IGNORE = "offlinefurnaces.ignore";
+            private static readonly List<string> _permissions = new List<string>
+            {
+                IGNORE,
+            };
+
+            public static void RegisterPermissions()
+            {
+                foreach (var permission in _permissions)
+                {
+                    _plugin.permission.RegisterPermission(permission, _plugin);
+                }
+            }
+
+            public static bool HasPermission(BasePlayer player, string permissionName)
+            {
+                return _plugin.permission.UserHasPermission(player.UserIDString, permissionName);
+            }
+        }
+
+        #endregion Permissions
+    }
+}
